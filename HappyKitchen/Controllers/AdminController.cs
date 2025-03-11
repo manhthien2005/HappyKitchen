26d198c7fba41261678ca53cdfb7cec8922ed4e1
CatS1mp <155413856+CatS1mp@users.noreply.github.com>
2025-03-11 10:14:01 +0700

    xóa type = "password " tránh lỗi overlay hiển thị
    - thêm tạm view Admin gồm:
    quản lý hóa đơn ( chưa kết nối dtb )

---
+++ HappyKitchen/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HappyKitchen.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult BillManagement()
+        {
+            return View();
+        }
+    }
+}
